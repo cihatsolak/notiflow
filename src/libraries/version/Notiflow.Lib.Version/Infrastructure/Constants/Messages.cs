45d1931e3c75dd6487ded5b4436171e1b195ffb0
Cihat Solak <cihatsolak@hotmail.com>
2023-02-09 21:58:28 +0300

    feat: add library for api versioning

---
+++ src/libraries/version/Notiflow.Lib.Version/Infrastructure/Constants/Messages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notiflow.Lib.Version.Infrastructure.Constants
+{
+    internal class Messages
+    {
+    }
+}
---
+++ src/libraries/version/Notiflow.Lib.Version/Infrastructure/Settings/VersionSetting.cs
@@ -0,0 +1,16 @@
+﻿namespace Notiflow.Lib.Version.Infrastructure.Settings
+{
+    public interface IApiVersionSetting
+    {
+        string HeaderName { get; init; }
+        int MajorVersion { get; init; }
+        int MinorVersion { get; init; }
+    }
+
+    public sealed record ApiVersionSetting : IApiVersionSetting
+    {
+        public string HeaderName { get; init; }
+        public int MajorVersion { get; init; }
+        public int MinorVersion { get; init; }
+    }
+}
