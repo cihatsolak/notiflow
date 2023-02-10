62482d03dfc1ef8a0c43a4120d1bf35df4d5e6c0
Cihat Solak <cihatsolak@hotmail.com>
2023-02-11 00:02:39 +0300

    feat: add library for cookie authorization and usage

---
+++ src/libraries/cookie/Notiflow.Lib.Cookie/Notiflow.Lib.Cookie/Middlewares/ApplicationBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Notiflow.Lib.Cookie.Middlewares
+{
+    public static class ApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// Use cookie policy options
+        /// </summary>
+        /// <param name="app">type of built-in application builder interface</param>
+        /// <returns>type of built-in application builder interface</returns>
+        public static IApplicationBuilder UseCookiePolicyOptions(this IApplicationBuilder app)
+        {
+            app.UseCookiePolicy(new CookiePolicyOptions
+            {
+                Secure = CookieSecurePolicy.Always,
+                HttpOnly = HttpOnlyPolicy.Always,
+                MinimumSameSitePolicy = SameSiteMode.Strict
+            });
+
+            return app;
+        }
+    }
+}
