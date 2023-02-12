64a15b9a1a02e20e891bc015a353b1b7df5c2565
Cihat Solak <cihatsolak@hotmail.com>
2023-02-13 00:02:51 +0300

    feat: add helper methods for token adventure

---
+++ src/libraries/auth/Notiflow.Lib.Auth/Infrastructure/Settings/JwtTokenSetting.cs
@@ -0,0 +1,20 @@
+﻿namespace Notiflow.Lib.Auth.Infrastructure.Settings
+{
+    public interface IJwtTokenSetting
+    {
+        List<string> Audiences { get; init; }
+        string Issuer { get; init; }
+        int AccessTokenExpiration { get; init; }
+        int RefreshTokenExpiration { get; init; }
+        string SecurityKey { get; init; }
+    }
+
+    internal sealed record JwtTokenSetting : IJwtTokenSetting
+    {
+        public List<string> Audiences { get; init; }
+        public string Issuer { get; init; }
+        public int AccessTokenExpiration { get; init; }
+        public int RefreshTokenExpiration { get; init; }
+        public string SecurityKey { get; init; }
+    }
+}
---
+++ src/libraries/auth/Notiflow.Lib.Auth/Usings.cs
@@ -0,0 +1,5 @@
+﻿global using Microsoft.IdentityModel.Tokens;
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
+global using System.Security.Cryptography;
+global using System.Text;
