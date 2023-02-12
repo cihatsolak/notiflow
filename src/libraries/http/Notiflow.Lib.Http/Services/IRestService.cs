496736fe9c546df9acd737c332da8b3f8b9d53e8
Cihat Solak <cihatsolak@hotmail.com>
2023-02-12 12:27:06 +0300

    refactor: revise methods of http library

--- src/libraries/http/Notiflow.Lib.Http/Services/IRestService.cs
+++ src/libraries/http/Notiflow.Lib.Http/Services/IRestService.cs
@@ -5,101 +5,101 @@
         /// <summary>
         /// Http get request
         /// </summary>
-        /// <typeparam name="TModel">object instantiable concrete class</typeparam>
+        /// <typeparam name="TResponse">object instantiable concrete class</typeparam>
         /// <param name="clientName">http client name registered in container</param>
         /// <param name="routeUrl">the route address to which the request will be made</param>
         /// <returns>specified, instantiable concrete class</returns>
         /// <exception cref="ArgumentNullException">thrown when http client type cannot be created</exception>
-        Task<TModel> GetResponseAsync<TModel>(string clientName, string routeUrl) where TModel : class, new();
+        Task<TResponse> GetResponseAsync<TResponse>(string clientName, string routeUrl) where TResponse : class, new();
 
         /// <summary>
         /// Http get request
         /// </summary>
-        /// <typeparam name="TModel">object instantiable concrete class</typeparam>
+        /// <typeparam name="TResponse">object instantiable concrete class</typeparam>
         /// <param name="clientName">http client name registered in container</param>
         /// <param name="routeUrl">the route address to which the request will be made</param>
-        /// <param name="headersCollection">header information to be sent with the request</param>
+        /// <param name="nameValueCollection">header information to be sent with the request</param>
         /// <returns>specified, instantiable concrete class</returns>
         /// <exception cref="ArgumentNullException">thrown when http client type cannot be created</exception>
-        Task<TModel> GetResponseAsync<TModel>(string clientName, string routeUrl, NameValueCollection headersCollection) where TModel : class, new();
+        Task<TResponse> GetResponseAsync<TResponse>(string clientName, string routeUrl, NameValueCollection nameValueCollection) where TResponse : class, new();
 
         /// <summary>
         /// Http post request
         /// </summary>
-        /// <typeparam name="TModel">object instantiable concrete class</typeparam>
+        /// <typeparam name="TResponse">object instantiable concrete class</typeparam>
         /// <param name="clientName">http client name registered in container</param>
         /// <param name="routeUrl">the route address to which the request will be made</param>
         /// <returns>specified, instantiable concrete class</returns>
         /// <exception cref="ArgumentNullException">thrown when http client type cannot be created</exception>
-        Task<TModel> PostResponseAsync<TModel>(string clientName, string routeUrl) where TModel : class, new();
+        Task<TResponse> PostResponseAsync<TResponse>(string clientName, string routeUrl) where TResponse : class, new();
 
         /// <summary>
         /// Http post request
         /// </summary>
-        /// <typeparam name="TModel">object instantiable concrete class</typeparam>
+        /// <typeparam name="TResponse">object instantiable concrete class</typeparam>
         /// <param name="clientName">http client name registered in container</param>
         /// <param name="routeUrl">the route address to which the request will be made</param>
         /// <param name="parameters">optional body information. (class to be posted)</param>
         /// <returns>specified, instantiable concrete class</returns>
         /// <exception cref="ArgumentNullException">thrown when http client type cannot be created</exception>
-        Task<TModel> PostResponseAsync<TModel>(string clientName, string routeUrl, object parameters) where TModel : class, new();
+        Task<TResponse> PostResponseAsync<TResponse>(string clientName, string routeUrl, object parameters) where TResponse : class, new();
 
         /// <summary>
         /// Http post request
         /// </summary>
-        /// <typeparam name="TModel">object instantiable concrete class</typeparam>
+        /// <typeparam name="TResponse">object instantiable concrete class</typeparam>
         /// <param name="clientName">http client name registered in container</param>
         /// <param name="routeUrl">the route address to which the request will be made</param>
         /// <param name="parameters">optional body information. (class to be posted)</param>
-        /// <param name="headersCollection">header information to be sent with the request</param>
+        /// <param name="nameValueCollection">header information to be sent with the request</param>
         /// <returns>specified, instantiable concrete class</returns>
         /// <exception cref="ArgumentNullException">thrown when http client type cannot be created</exception>
-        Task<TModel> PostResponseAsync<TModel>(string clientName, string routeUrl, object parameters, NameValueCollection headersCollection) where TModel : class, new();
+        Task<TResponse> PostResponseAsync<TResponse>(string clientName, string routeUrl, object parameters, NameValueCollection nameValueCollection) where TResponse : class, new();
 
         /// <summary>
         /// Http post request - multipart form data content
         /// </summary>
-        /// <typeparam name="TModel">object instantiable concrete class</typeparam>
+        /// <typeparam name="TResponse">object instantiable concrete class</typeparam>
         /// <param name="clientName">http client name registered in container</param>
         /// <param name="routeUrl">the route address to which the request will be made</param>
         /// <param name="multipartFormDataContent">multipart form data content to be submitted</param>
         /// <returns>specified, instantiable concrete class</returns>
         /// <exception cref="ArgumentNullException">thrown when http client type cannot be created</exception>
-        Task<TModel> PostMultipartDataResponseAsync<TModel>(string clientName, string routeUrl, MultipartFormDataContent multipartFormDataContent) where TModel : class, new();
+        Task<TResponse> PostMultipartDataResponseAsync<TResponse>(string clientName, string routeUrl, MultipartFormDataContent multipartFormDataContent) where TResponse : class, new();
 
         /// <summary>
         /// Http post request - multipart form data content
         /// </summary>
-        /// <typeparam name="TModel">object instantiable concrete class</typeparam>
+        /// <typeparam name="TResponse">object instantiable concrete class</typeparam>
         /// <param name="clientName">http client name registered in container</param>
         /// <param name="routeUrl">the route address to which the request will be made</param>
         /// <param name="multipartFormDataContent">multipart form data content to be submitted</param>
-        /// <param name="headersCollection">header information to be sent with the request</param>
+        /// <param name="nameValueCollection">header information to be sent with the request</param>
         /// <returns>specified, instantiable concrete class</returns>
         /// <exception cref="ArgumentNullException">thrown when http client type cannot be created</exception>
-        Task<TModel> PostMultipartDataResponseAsync<TModel>(string clientName, string routeUrl, MultipartFormDataContent multipartFormDataContent, NameValueCollection headersCollection) where TModel : class, new();
+        Task<TResponse> PostMultipartDataResponseAsync<TResponse>(string clientName, string routeUrl, MultipartFormDataContent multipartFormDataContent, NameValueCollection nameValueCollection) where TResponse : class, new();
 
         /// <summary>
         /// Http post request - form url encoded content
         /// </summary>
-        /// <typeparam name="TModel">object instantiable concrete class</typeparam>
+        /// <typeparam name="TResponse">object instantiable concrete class</typeparam>
         /// <param name="clientName">http client name registered in container</param>
         /// <param name="routeUrl">the route address to which the request will be made</param>
         /// <param name="keyValuePairs">encoded list to be sent with the request</param>
         /// <returns>specified, instantiable concrete class</returns>
         /// <exception cref="ArgumentNullException">thrown when http client type cannot be created</exception>
-        Task<TModel> PostEncodedResponseAsync<TModel>(string clientName, string routeUrl, IList<KeyValuePair<string, string>> keyValuePairs) where TModel : class, new();
+        Task<TResponse> PostEncodedResponseAsync<TResponse>(string clientName, string routeUrl, IList<KeyValuePair<string, string>> keyValuePairs) where TResponse : class, new();
 
         /// <summary>
         /// Http post request - form url encoded content
         /// </summary>
-        /// <typeparam name="TModel">object instantiable concrete class</typeparam>
+        /// <typeparam name="TResponse">object instantiable concrete class</typeparam>
         /// <param name="clientName">http client name registered in container</param>
         /// <param name="routeUrl">the route address to which the request will be made</param>
         /// <param name="keyValuePairs">encoded list to be sent with the request</param>
-        /// <param name="headersCollection">header information to be sent with the request</param>
+        /// <param name="nameValueCollection">header information to be sent with the request</param>
         /// <returns>specified, instantiable concrete class</returns>
         /// <exception cref="ArgumentNullException">thrown when http client type cannot be created</exception>
-        Task<TModel> PostEncodedResponseAsync<TModel>(string clientName, string routeUrl, IList<KeyValuePair<string, string>> keyValuePairs, NameValueCollection headersCollection) where TModel : class, new();
+        Task<TResponse> PostEncodedResponseAsync<TResponse>(string clientName, string routeUrl, IList<KeyValuePair<string, string>> keyValuePairs, NameValueCollection nameValueCollection) where TResponse : class, new();
     }
 }
--- src/libraries/http/Notiflow.Lib.Http/Usings.cs
+++ src/libraries/http/Notiflow.Lib.Http/Usings.cs
@@ -1,3 +1,9 @@
-﻿global using Microsoft.Net.Http.Headers;
-global using Notiflow.Lib.Http.Infrastructure.Constants;
+﻿global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.DependencyInjection.Extensions;
+global using Microsoft.Net.Http.Headers;
+global using Notiflow.Lib.Http.Services;
+global using Polly;
+global using Polly.Extensions.Http;
+global using Serilog;
 global using System.Collections.Specialized;
+global using System.Net.Http.Json;
