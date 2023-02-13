e3333c224fd5cc7df196b773be0335439d403497
Cihat Solak <cihatsolak@hotmail.com>
2023-02-14 00:16:31 +0300

    feat: explore the types of claims in tokens

---
+++ src/libraries/auth/Notiflow.Lib.Auth/Infrastructure/Constants/Messages.cs
@@ -0,0 +1,13 @@
+﻿namespace Notiflow.Lib.Auth.Infrastructure.Constants
+{
+    internal static class ExceptionMessage
+    {
+        internal const string ClaimTypeEmailRequired = "Email request type not found.";
+        internal const string ClaimTypeNameRequired = "Name claim type not found.";
+        internal const string ClaimTypeNameIdentifierRequired = "Name identifier (id) claim type not found.";
+        internal const string ClaimTypeRoleRequired = "The role claim type could not be found.";
+        internal const string ClaimTypeJtiRequired = "Jti unique key claim type not found.";
+        internal const string ClaimTypeAudienceRequired = "The provider claim type could not be found.";
+        internal const string ClaimTypeUsernameRequired = "Username claim type not found.";
+    }
+}
--- src/libraries/auth/Notiflow.Lib.Auth/Services/ClaimManager.cs
+++ src/libraries/auth/Notiflow.Lib.Auth/Services/ClaimManager.cs
@@ -1,12 +1,102 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Notiflow.Lib.Auth.Services
+﻿namespace Notiflow.Lib.Auth.Services
 {
-    internal class ClaimManager : IClaimService
+    public sealed class ClaimManager : IClaimService
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ClaimManager(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string EmailAddress => GetEmailAddress();
+        public string Name => GetName();
+        public int UserId => GetUserId();
+        public string Role => GetRole();
+        public string Jti => GetJti();
+        public List<string> Audiences => GetAudiences();
+        public string Audience => GetAudience();
+        public string Username => GetUsername();
+
+        private string GetEmailAddress()
+        {
+            string emailAddress = _httpContextAccessor.HttpContext.User.Claims.FirstOrDefault(p => p.Type.Equals(ClaimTypes.Email))?.Value;
+            if (string.IsNullOrWhiteSpace(emailAddress))
+                throw new ClaimException(ExceptionMessage.ClaimTypeEmailRequired);
+
+            return emailAddress;
+        }
+
+        private string GetName()
+        {
+            string name = _httpContextAccessor.HttpContext.User.Claims.FirstOrDefault(p => p.Type.Equals(ClaimTypes.Name))?.Value;
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ClaimException(ExceptionMessage.ClaimTypeNameRequired);
+
+            return name;
+        }
+
+        private int GetUserId()
+        {
+            string nameIdentifier = _httpContextAccessor.HttpContext.User.Claims.FirstOrDefault(p => p.Type.Equals(ClaimTypes.NameIdentifier))?.Value;
+            if (string.IsNullOrWhiteSpace(nameIdentifier))
+                throw new ClaimException(ExceptionMessage.ClaimTypeNameIdentifierRequired);
+
+            if (!int.TryParse(nameIdentifier, out int userId))
+                throw new ClaimException(ExceptionMessage.ClaimTypeNameIdentifierRequired);
+
+            if (0 >= userId)
+                throw new ClaimException(ExceptionMessage.ClaimTypeNameIdentifierRequired);
+
+            return userId;
+        }
+
+        private string GetRole()
+        {
+            string roleName = _httpContextAccessor.HttpContext.User.Claims.FirstOrDefault(p => p.Type.Equals(ClaimTypes.Role))?.Value;
+            if (string.IsNullOrWhiteSpace(roleName))
+                throw new ClaimException(ExceptionMessage.ClaimTypeRoleRequired);
+
+            return roleName;
+        }
+
+        private string GetJti()
+        {
+            string jti = _httpContextAccessor.HttpContext.User.Claims.FirstOrDefault(p => p.Type.Equals(JwtRegisteredClaimNames.Jti))?.Value;
+            if (string.IsNullOrWhiteSpace(jti))
+                throw new ClaimException(ExceptionMessage.ClaimTypeJtiRequired);
+
+            return jti;
+        }
+
+        private List<string> GetAudiences()
+        {
+            List<string> audiences = _httpContextAccessor.HttpContext.User.Claims
+                    .Where(p => p.Type.Equals(JwtRegisteredClaimNames.Aud))
+                    .Select(p => p.Value).ToList();
+
+            if (audiences is null || !audiences.Any())
+                throw new ClaimException(ExceptionMessage.ClaimTypeAudienceRequired);
+
+            return audiences;
+        }
+
+        private string GetAudience()
+        {
+            string audience = _httpContextAccessor.HttpContext.User.Claims.FirstOrDefault(p => p.Type.Equals(JwtRegisteredClaimNames.Aud))?.Value;
+            if (string.IsNullOrWhiteSpace(audience))
+                throw new ClaimException(ExceptionMessage.ClaimTypeAudienceRequired);
+
+            return audience;
+        }
+
+        private string GetUsername()
+        {
+            string username = _httpContextAccessor.HttpContext.User.Claims.FirstOrDefault(p => p.Type.Equals(ClaimTypes.GivenName))?.Value;
+            if (string.IsNullOrWhiteSpace(username))
+                throw new ClaimException(ExceptionMessage.ClaimTypeUsernameRequired);
+
+            return username;
+        }
     }
 }
