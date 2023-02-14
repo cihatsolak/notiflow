26207226c1addfefac71fc198325eb709c1f50ca
Cihat Solak <cihatsolak@hotmail.com>
2023-02-14 20:45:27 +0300

    feat: add redis retry policies with polly library

--- src/libraries/cache/Notiflow.Lib.Cache/Usings.cs
+++ src/libraries/cache/Notiflow.Lib.Cache/Usings.cs
@@ -1,3 +1,6 @@
-﻿global using System.ComponentModel;
-global using Notiflow.Lib.Cache.Constants.Enums;
+﻿global using Notiflow.Lib.Cache.Constants.Enums;
+global using Polly;
+global using Polly.Retry;
+global using Serilog;
 global using StackExchange.Redis;
+global using System.ComponentModel;
