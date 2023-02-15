18692cc154ee95c51aa7829ce9e1aa6d8db9dc71
Cihat Solak <cihatsolak@hotmail.com>
2023-02-15 20:41:00 +0300

    feat: add common business unit class

---
+++ src/libraries/database/Notiflow.Lib.Database/Infrastructure/Exceptions/DatabaseOperationException.cs
@@ -0,0 +1,22 @@
+﻿namespace Notiflow.Lib.Database.Infrastructure.Exceptions
+{
+    [Serializable]
+    public sealed class DatabaseOperationException : Exception
+    {
+        public DatabaseOperationException()
+        {
+        }
+
+        public DatabaseOperationException(string message) : base(message)
+        {
+        }
+
+        private DatabaseOperationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public DatabaseOperationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
--- src/libraries/database/Notiflow.Lib.Database/Usings.cs
+++ src/libraries/database/Notiflow.Lib.Database/Usings.cs
@@ -1,1 +1,6 @@
-﻿global using System.Linq.Expressions;
+﻿global using Microsoft.EntityFrameworkCore;
+global using Notiflow.Lib.Database.Abstract;
+global using Notiflow.Lib.Database.Infrastructure.Exceptions;
+global using Serilog;
+global using System.Linq.Expressions;
+global using System.Runtime.Serialization;
