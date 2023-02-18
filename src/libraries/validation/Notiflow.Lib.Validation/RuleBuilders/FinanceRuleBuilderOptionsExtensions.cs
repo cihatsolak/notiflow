73b6649c922899cd52117d4e219a15b9184302b9
Cihat Solak <cihatsolak@hotmail.com>
2023-02-18 15:12:34 +0300

    feat: add finance, personnel and security rule extensions

---
+++ src/libraries/validation/Notiflow.Lib.Validation/RuleBuilders/FinanceRuleBuilderOptionsExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace Notiflow.Lib.Validation.RuleBuilders
+{
+    public static class FinanceRuleBuilderOptionsExtensions
+    {
+        /// <summary>
+        /// Checks if the user's credit card number is correct
+        /// </summary>
+        /// <typeparam name="TClass">class to validate</typeparam>
+        /// <param name="ruleBuilder">rule builder</param>
+        /// <param name="errorMessage">error message</param>
+        /// <returns>type of rule builder options</returns>
+        public static IRuleBuilderOptions<TClass, string> CreditCard<TClass>(this IRuleBuilder<TClass, string> ruleBuilder, string errorMessage) where TClass : class, new()
+        {
+            return ruleBuilder
+                .NotEmpty().WithMessage(errorMessage)
+                .NotNull().WithMessage(errorMessage)
+                .Length(13, 19).WithMessage(errorMessage)
+                .Matches(RegularExpressions.CreditCard).WithMessage(errorMessage);
+        }
+
+        /// <summary>
+        /// Checks if the tax number is correct
+        /// </summary>
+        /// <typeparam name="TClass">class to validate</typeparam>
+        /// <param name="ruleBuilder">rule builder</param>
+        /// <param name="errorMessage">error message</param>
+        /// <returns>type of rule builder options</returns>
+        public static IRuleBuilderOptions<TClass, string> CreditOrDebitCard<TClass>(this IRuleBuilder<TClass, string> ruleBuilder, string errorMessage) where TClass : class, new()
+        {
+            return ruleBuilder
+                .NotEmpty().WithMessage(errorMessage)
+                .NotNull().WithMessage(errorMessage)
+                .Length(10).WithMessage(errorMessage)
+                .Matches(RegularExpressions.TaxNumber).WithMessage(errorMessage);
+        }
+    }
+}
--- src/libraries/validation/Notiflow.Lib.Validation/RuleBuilders/PersonalRuleBuilderOptionsExtensions.cs
+++ src/libraries/validation/Notiflow.Lib.Validation/RuleBuilders/PersonalRuleBuilderOptionsExtensions.cs
@@ -1,12 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Notiflow.Lib.Validation.RuleBuilders
+﻿namespace Notiflow.Lib.Validation.RuleBuilders
 {
     public static class PersonalRuleBuilderOptionsExtensions
     {
+        /// <summary>
+        /// Checks if the user's ID number is correct
+        /// </summary>
+        /// <typeparam name="TClass">class to validate</typeparam>
+        /// <param name="ruleBuilder">rule builder</param>
+        /// <param name="errorMessage">error message</param>
+        /// <returns>type of rule builder options</returns>
+        public static IRuleBuilderOptions<TClass, string> TcNumber<TClass>(this IRuleBuilder<TClass, string> ruleBuilder, string errorMessage) where TClass : class, new()
+        {
+            return ruleBuilder
+                .NotEmpty().WithMessage(errorMessage)
+                .NotNull().WithMessage(errorMessage)
+                .Length(11).WithMessage(errorMessage)
+                .Matches(RegularExpressions.TcNumber).WithMessage(errorMessage);
+        }
+
+        /// <summary>
+        /// Checks if the user's landline number is correct
+        /// </summary>
+        /// <remarks>The area code is included and the number must begin with a zero.</remarks>
+        /// <typeparam name="TClass">class to validate</typeparam>
+        /// <param name="ruleBuilder">rule builder</param>
+        /// <param name="errorMessage">error message</param>
+        /// <returns>type of rule builder options</returns>
+        public static IRuleBuilderOptions<TClass, string> Landline<TClass>(this IRuleBuilder<TClass, string> ruleBuilder, string errorMessage) where TClass : class, new()
+        {
+            return ruleBuilder
+                .NotEmpty().WithMessage(errorMessage)
+                .NotNull().WithMessage(errorMessage)
+                .Length(11).WithMessage(errorMessage)
+                .Matches(RegularExpressions.Landline).WithMessage(errorMessage);
+        }
+
+        /// <summary>
+        /// Checks if the user's mobiel phone number is correct
+        /// </summary>
+        /// <remarks>The area code is included and the number must begin with a zero.</remarks>
+        /// <typeparam name="TClass">class to validate</typeparam>
+        /// <param name="ruleBuilder">rule builder</param>
+        /// <param name="errorMessage">error message</param>
+        /// <returns>type of rule builder options</returns>
+        public static IRuleBuilderOptions<TClass, string> MobilePhone<TClass>(this IRuleBuilder<TClass, string> ruleBuilder, string errorMessage) where TClass : class, new()
+        {
+            return ruleBuilder
+                .NotEmpty().WithMessage(errorMessage)
+                .NotNull().WithMessage(errorMessage)
+                .Length(10).WithMessage(errorMessage)
+                .Matches(RegularExpressions.MobilePhone).WithMessage(errorMessage);
+        }
     }
 }
