dc0a642fcfd6abd799be58726d43d6f9404742a6
Cihat Solak <cihatsolak@hotmail.com>
2023-02-27 21:50:15 +0300

    feat: add app health checks

---
+++ src/libraries/healthcheck/Notiflow.Lib.HealthCheck/Checks/ServiceConnectionHealthCheck.cs
@@ -0,0 +1,19 @@
+﻿namespace Notiflow.Lib.HealthCheck.Checks
+{
+    internal static class ServiceConnectionHealthCheck
+    {
+        internal static IHealthChecksBuilder AddServicesCheck(this IHealthChecksBuilder healthChecksBuilder, IHealthChecksUrlGroupSetting healthChecksUrlGroupSetting)
+        {
+            foreach (var serviceInfo in healthChecksUrlGroupSetting.UrlGroups)
+            {
+                healthChecksBuilder.AddUrlGroup(
+                    uri: serviceInfo.ServiceUri,
+                    name: serviceInfo.Name,
+                    failureStatus: HealthStatus.Unhealthy,
+                    tags: serviceInfo.Tags);
+            }
+
+            return healthChecksBuilder;
+        }
+    }
+}
---
+++ src/libraries/healthcheck/Notiflow.Lib.HealthCheck/Infrastructure/Settings/IHealthSetting.cs
@@ -0,0 +1,30 @@
+﻿namespace Notiflow.Lib.HealthCheck.Infrastructure.Settings
+{
+    internal interface IHealthSetting
+    {
+        string Name { get; init; }
+        string Uri { get; init; }
+        string Payload { get; init; }
+        string RestorePayload { get; init; }
+        int EvaluationTimeInSeconds { get; init; }
+        int ApiMaxActiveRequests { get; init; }
+        int MaximumHistoryEntriesPerEndpoint { get; init; }
+        string ResponsePath { get; init; }
+        string ViewUrl { get; init; }
+        string CustomCssPath { get; init; }
+    }
+
+    internal sealed record HealthSetting : IHealthSetting
+    {
+        public string Name { get; init; }
+        public string Uri { get; init; }
+        public string Payload { get; init; }
+        public string RestorePayload { get; init; }
+        public int EvaluationTimeInSeconds { get; init; }
+        public int ApiMaxActiveRequests { get; init; }
+        public int MaximumHistoryEntriesPerEndpoint { get; init; }
+        public string ResponsePath { get; init; }
+        public string ViewUrl { get; init; }
+        public string CustomCssPath { get; init; }
+    }
+}
