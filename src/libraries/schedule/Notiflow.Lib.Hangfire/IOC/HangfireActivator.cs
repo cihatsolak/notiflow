ca8535416091a3bfa36b452a2c6902cda29a84b7
Cihat Solak <cihatsolak@hotmail.com>
2023-02-27 21:20:15 +0300

    feat: create hangfire configurations

---
+++ src/libraries/schedule/Notiflow.Lib.Hangfire/IOC/HangfireActivator.cs
@@ -0,0 +1,17 @@
+﻿namespace Notiflow.Lib.Hangfire.IOC
+{
+    internal sealed class HangfireActivator : JobActivator
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public HangfireActivator(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public override object ActivateJob(Type jobType)
+        {
+            return _serviceProvider.GetService(jobType);
+        }
+    }
+}
---
+++ src/libraries/schedule/Notiflow.Lib.Hangfire/Infrastructure/Constants/Attempts.cs
@@ -0,0 +1,33 @@
+﻿namespace Notiflow.Lib.Hangfire.Infrastructure.Constants
+{
+    /// <summary>
+    /// How many times will the job be tried when it fails?
+    /// </summary>
+    public static class Attempts
+    {
+        /// <summary>
+        /// Try it one time
+        /// </summary>
+        public const int TryOne = 1;
+
+        /// <summary>
+        /// Try it twice
+        /// </summary>
+        public const int TryTwice = 2;
+
+        /// <summary>
+        /// Try it three times
+        /// </summary>
+        public const int TryThreeTimes = 3;
+
+        /// <summary>
+        /// Try it four times
+        /// </summary>
+        public const int TryFourTimes = 4;
+
+        /// <summary>
+        /// Try it five times
+        /// </summary>
+        public const int TryFiveTimes = 5;
+    }
+}
