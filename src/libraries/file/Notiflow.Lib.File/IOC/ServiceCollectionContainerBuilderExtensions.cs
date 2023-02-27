9ab8a17121e197943aecc53cca98f4e5c91786fe
Cihat Solak <cihatsolak@hotmail.com>
2023-02-27 21:32:42 +0300

    feat: add ftp library for file operations

---
+++ src/libraries/file/Notiflow.Lib.File/IOC/ServiceCollectionContainerBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Notiflow.Lib.File.IOC
+{
+    public static class ServiceCollectionContainerBuilderExtensions
+    {
+        /// <summary>
+        /// Add ftp setting
+        /// </summary>
+        /// <param name="services">type of built-in service collection interface</param>
+        /// <returns>type of built-in service collection interface</returns>
+        /// <exception cref="ArgumentNullException">thrown when the service provider cannot be built</exception>
+        public static void AddFtpSetting(this IServiceCollection services)
+        {
+            var serviceProvider = services.BuildServiceProvider();
+            ArgumentNullException.ThrowIfNull(serviceProvider);
+
+            var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+            services.Configure<FtpSetting>(configuration.GetRequiredSection(nameof(FtpSetting)));
+            services.TryAddSingleton<IFtpSetting>(provider => provider.GetRequiredService<IOptions<FtpSetting>>().Value);
+        }
+    }
+}
