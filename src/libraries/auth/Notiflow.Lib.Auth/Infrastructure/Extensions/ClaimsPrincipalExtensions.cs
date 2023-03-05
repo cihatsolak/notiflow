97948cabf98586b99a93fb4962043e8434d86da0
Cihat Solak <cihatsolak@hotmail.com>
2023-03-05 20:26:09 +0300

    refactor: update method naming and summary information

--- src/libraries/auth/Notiflow.Lib.Auth/Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
+++ src/libraries/auth/Notiflow.Lib.Auth/Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
@@ -1,27 +1,29 @@
 ﻿namespace Notiflow.Lib.Auth.Infrastructure.Extensions
 {
+    /// <summary>
+    /// Extension methods for configure claim principal in an <see cref="ClaimsPrincipal" />.
+    /// </summary>
     public static class ClaimsPrincipalExtensions
     {
         /// <summary>
-        /// Get all claim value by claim type
+        /// Lists claim values for claim type in claims principal
         /// </summary>
-        /// <remarks>Lists values based on the specified claim type. For example: Role</remarks>
         /// <param name="claimsPrincipal">type of claims principal</param>
-        /// <param name="claimType">claim type name</param>
-        /// <returns>claim value list</returns>
+        /// <param name="claimType">claim type</param>
+        /// <returns>filtered claim value list</returns>
         /// <exception cref="ArgumentNullException">thrown when claims principal is null or claim type is null|empty</exception>
+        /// <exception cref="ArgumentException">thrown when claim type is empty or null</exception>
         public static IEnumerable<string> Claims(this ClaimsPrincipal claimsPrincipal, string claimType)
         {
-            ArgumentException.ThrowIfNullOrEmpty(claimType);
             ArgumentNullException.ThrowIfNull(claimsPrincipal);
-
+            ArgumentException.ThrowIfNullOrEmpty(claimType);
+           
             return claimsPrincipal.FindAll(claimType).Select(x => x.Value);
         }
 
         /// <summary>
-        /// Get claim roles
+        /// Lists roles in claims principal
         /// </summary>
-        /// <remarks>lists claim roles</remarks>
         /// <param name="claimsPrincipal">type of claims principal</param>
         /// <returns>role list</returns>
         /// <exception cref="ArgumentNullException">thrown when claims principal is null</exception>
@@ -33,11 +35,10 @@
         }
 
         /// <summary>
-        /// Get claim audiences
+        /// Lists audiences in claim principal
         /// </summary>
-        /// <remarks>lists audiences</remarks>
         /// <param name="claimsPrincipal">type of claims principal</param>
-        /// <returns>lists audiences</returns>
+        /// <returns>audiences list </returns>
         /// <exception cref="ArgumentNullException">thrown when claims principal is null</exception>
         public static IEnumerable<string> ClaimAudiences(this ClaimsPrincipal claimsPrincipal)
         {
