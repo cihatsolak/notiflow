6c145cd9a7ea1a31971742653d646bba3f4a52fb
Cihat Solak <cihatsolak@hotmail.com>
2023-03-05 15:32:41 +0300

    feat: create response library for common response of services

---
+++ src/libraries/response/Notiflow.Lib.Response/Controllers/MainController.cs
@@ -0,0 +1,11 @@
+﻿namespace Notiflow.Lib.Response.Controllers
+{
+    [ApiController]
+    [Produces(MediaTypeNames.Application.Json)]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public class MainController : ControllerBase
+    {
+    }
+}
---
+++ src/libraries/response/Notiflow.Lib.Response/Models/ResponseModel.cs
@@ -0,0 +1,82 @@
+﻿namespace Notiflow.Lib.Response.Models
+{
+    public class ResponseModel<TResponseData>
+    {
+        public bool Succeeded { get; init; }
+        public TResponseData Data { get; init; }
+        public int StatusCode { get; init; }
+        public string StatusMessage { get; init; }
+        public List<string> Errors { get; init; }
+
+        public static ResponseModel<TResponseData> Success(int statusCode)
+        {
+            return new ResponseModel<TResponseData>
+            {
+                Data = default,
+                StatusCode = statusCode,
+                Succeeded = true
+            };
+        }
+
+        public static ResponseModel<TResponseData> Success(TResponseData data)
+        {
+            return new ResponseModel<TResponseData>
+            {
+                Data = data,
+                StatusCode = 9001,
+                Succeeded = true
+            };
+        }
+
+        public static ResponseModel<TResponseData> Success(int statusCode, TResponseData data)
+        {
+            return new ResponseModel<TResponseData>
+            {
+                Data = data,
+                StatusCode = statusCode,
+                Succeeded = true
+            };
+        }
+
+        public static ResponseModel<TResponseData> Success(int statusCode, string statusMessage, TResponseData data)
+        {
+            return new ResponseModel<TResponseData>
+            {
+                Data = data,
+                StatusCode = statusCode,
+                Succeeded = true,
+                StatusMessage = statusMessage
+            };
+        }
+
+        public static ResponseModel<TResponseData> Fail(int statusCode)
+        {
+            return new ResponseModel<TResponseData>
+            {
+                Errors = default,
+                StatusCode = statusCode,
+                Succeeded = false
+            };
+        }
+
+        public static ResponseModel<TResponseData> Fail(int statusCode, string error)
+        {
+            return new ResponseModel<TResponseData>
+            {
+                Errors = new List<string>() { error },
+                StatusCode = statusCode,
+                Succeeded = false
+            };
+        }
+
+        public static ResponseModel<TResponseData> Fail(int statusCode, List<string> errors)
+        {
+            return new ResponseModel<TResponseData>
+            {
+                Errors = errors,
+                StatusCode = statusCode,
+                Succeeded = false
+            };
+        }
+    }
+}
