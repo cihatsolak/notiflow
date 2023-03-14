abd13c1e122eb5052f074ce6ea0a24b7244fa50a
Cihat Solak <cihatsolak@hotmail.com>
2023-03-14 20:24:25 +0300

    feat: add extension to verify tax number

--- src/libraries/validation/Puzzle.Lib.Validation/RuleBuilders/FinanceRuleBuilderOptionsExtensions.cs
+++ src/libraries/validation/Puzzle.Lib.Validation/RuleBuilders/FinanceRuleBuilderOptionsExtensions.cs
@@ -37,5 +37,21 @@
                 .Length(10, 19).WithMessage(errorMessage)
                 .Matches(RegularExpressions.CreditCard).WithMessage(errorMessage);
         }
+
+        /// <summary>
+        /// Adds validation rules for a Turkish tax number, which must be 10 characters long.
+        /// </summary>
+        /// <typeparam name="TClass">The type of the class being validated.</typeparam>
+        /// <param name="ruleBuilder">The rule builder.</param>
+        /// <param name="errorMessage">The error message to display if the validation fails.</param>
+        /// <returns>The rule builder options.</returns>
+        public static IRuleBuilderOptions<TClass, string> TaxNumber<TClass>(this IRuleBuilder<TClass, string> ruleBuilder, string errorMessage) where TClass : class, new()
+        {
+            return ruleBuilder
+                .NotEmpty().WithMessage(errorMessage)
+                .NotNull().WithMessage(errorMessage)
+                .Length(10).WithMessage(errorMessage)
+                .Matches(RegularExpressions.TaxNumber).WithMessage(errorMessage);
+        }
     }
 }
