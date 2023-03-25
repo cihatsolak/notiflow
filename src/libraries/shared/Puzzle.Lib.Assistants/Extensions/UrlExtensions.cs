33903ba9b730e2ad9fceaf04580a0b4901aadd0b
Cihat Solak <cihatsolak@hotmail.com>
2023-03-25 15:57:22 +0300

    fet: add url extension

---
+++ src/libraries/shared/Puzzle.Lib.Assistants/Extensions/UrlExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Puzzle.Lib.Assistants.Extensions
+{
+    public static class UrlExtensions
+    {
+        public static string ToSlugUrl(this string text)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(text);
+
+            return PrepareUrl(text);
+        }
+
+        private static string PrepareUrl(string text)
+        {
+            text = text.ToLowerInvariant();
+
+            var bytes = Encoding.GetEncoding("Cyrillic").GetBytes(text);
+            text = Encoding.ASCII.GetString(bytes);
+            text = Regex.Replace(text, @"\s", "-", RegexOptions.Compiled);
+            text = Regex.Replace(text, @"[^a-z0-9\s-_]", "", RegexOptions.Compiled);
+            text = text.Trim('-', '_');
+            text = Regex.Replace(text, @"([-_]){2,}", "$1", RegexOptions.Compiled);
+
+            return text;
+        }
+    }
+}
--- src/libraries/shared/Puzzle.Lib.Assistants/Usings.cs
+++ src/libraries/shared/Puzzle.Lib.Assistants/Usings.cs
@@ -2,5 +2,6 @@
 global using System.ComponentModel;
 global using System.Globalization;
 global using System.Reflection;
+global using System.Text;
 global using System.Text.Json;
 global using System.Text.RegularExpressions;
