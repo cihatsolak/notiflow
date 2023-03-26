b3967842f2b3cc4e4cd478489a2462ffb9ab576a
Cihat Solak <cihatsolak@hotmail.com>
2023-03-26 21:20:43 +0300

    fix: add serilog middleware

--- src/libraries/logging/Puzzle.Lib.Logging/Configurations/MicrosoftTeamsLoggerConfiguration.cs
+++ src/libraries/logging/Puzzle.Lib.Logging/Configurations/MicrosoftTeamsLoggerConfiguration.cs
@@ -5,8 +5,8 @@
         internal static LoggerConfiguration WriteToMicrosoftTeams(this LoggerConfiguration loggerConfiguration)
         {
             MicrosoftTeamsSinkOptions microsoftTeamsSinkOptions =
-                new("https://dogusgrubu.webhook.office.com/webhookb2/1edb7160-0da7-43f6-8e85-cc9fec8918a6@cc76235c-86ab-4979-bc0b-0e78c66edb7c/IncomingWebhook/af8fe6f7834b4962b782149e0861eb42/8f0e1325-56da-461c-a59c-f181a9b60b7e",
-                    "Sistemsel bir hata oluştu.");
+                new("Hook!",
+                    "A system error has occurred.");
 
             loggerConfiguration.WriteTo.MicrosoftTeams(
                 microsoftTeamsSinkOptions: microsoftTeamsSinkOptions,
