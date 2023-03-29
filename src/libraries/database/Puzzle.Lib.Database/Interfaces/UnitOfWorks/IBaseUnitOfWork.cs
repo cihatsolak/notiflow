df17dec9220b8d191fbc88dc165618dd9e9b44f2
Cihat Solak <cihatsolak@hotmail.com>
2023-03-29 22:04:09 +0300

    refactor: consider soft delete when saving changes

--- src/libraries/database/Puzzle.Lib.Database/Interfaces/UnitOfWorks/IBaseUnitOfWork.cs
+++ src/libraries/database/Puzzle.Lib.Database/Interfaces/UnitOfWorks/IBaseUnitOfWork.cs
@@ -9,17 +9,10 @@
     public interface IBaseUnitOfWork
     {
         /// <summary>
-        /// Asynchronously saves changes to the database.
-        /// </summary>
-        /// <param name="cancellationToken">A cancellation token that can be used to cancel the operation.</param>
-        /// <returns>A task representing the asynchronous save operation, which returns the number of objects written to the database.</returns>
-        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
-
-        /// <summary>
         /// Asynchronously saves changes to a shadow database for auditing purposes.
         /// </summary>
         /// <param name="cancellationToken">A cancellation token that can be used to cancel the operation.</param>
         /// <returns>A task representing the asynchronous save operation, which returns the number of objects written to the shadow database.</returns>
-        Task<int> SaveChangesShadowAsync(CancellationToken cancellationToken = default);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }
 }
