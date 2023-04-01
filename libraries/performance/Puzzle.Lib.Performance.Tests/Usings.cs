bb3d0488b33c15cb784cccd3b60418c5d74bb7ed
Cihat Solak <cihatsolak@hotmail.com>
2023-04-02 00:48:40 +0300

    test: add test code for performance library

---
+++ libraries/performance/Puzzle.Lib.Performance.Tests/Usings.cs
@@ -0,0 +1,7 @@
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.AspNetCore.TestHost;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Puzzle.Lib.Performance.IOC;
+global using Puzzle.Lib.Performance.Middlewares;
+global using Xunit;
