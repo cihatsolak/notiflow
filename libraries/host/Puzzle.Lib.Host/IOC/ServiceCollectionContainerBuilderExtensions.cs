f7fac791f9a1bd84b706f90114c038cb738d1b7a
Cihat Solak <cihatsolak@hotmail.com>
2023-04-02 00:24:34 +0300

    test: add test code for host library

--- libraries/host/Puzzle.Lib.Host/IOC/ServiceCollectionContainerBuilderExtensions.cs
+++ libraries/host/Puzzle.Lib.Host/IOC/ServiceCollectionContainerBuilderExtensions.cs
@@ -1,10 +1,26 @@
-﻿namespace Puzzle.Lib.Host.IOC
+﻿
+namespace Puzzle.Lib.Host.IOC
 {
     /// <summary>
     /// This class provides a set of extension methods for adding additional functionality to the IServiceCollection and ContainerBuilder instances.
     /// </summary>
     public static class ServiceCollectionContainerBuilderExtensions
     {
+        /// <summary>
+        /// Adds tag helper initializers for the ScriptTagHelper, LinkTagHelper, and ImageTagHelper classes to the specified <see cref="IServiceCollection"/>.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the tag helper initializers to.</param>
+        /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
+
+        public static IServiceCollection AddTagHelperInitializer(this IServiceCollection services)
+        {
+            services.AddSingleton<ITagHelperInitializer<ScriptTagHelper>, ScriptVersionTagHelperInitializer>();
+            services.AddSingleton<ITagHelperInitializer<LinkTagHelper>, StyleVersionTagHelperInitializer>();
+            services.AddSingleton<ITagHelperInitializer<ImageTagHelper>, ImageVersionTagHelperInitializer>();
+
+            return services;
+        }
+
         /// <summary>
         /// Adds route settings to the provided IServiceCollection instance by configuring the routing options.
         /// </summary>
--- libraries/host/Puzzle.Lib.Host/Usings.cs
+++ libraries/host/Puzzle.Lib.Host/Usings.cs
@@ -1,3 +1,8 @@
-﻿global using Microsoft.Extensions.Configuration;
+﻿global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Mvc.Razor;
+global using Microsoft.AspNetCore.Mvc.Rendering;
+global using Microsoft.AspNetCore.Mvc.TagHelpers;
+global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Hosting;
+global using Puzzle.Lib.Host.TagHelpers;
