d6f4477cd37ad7e42f54293b078967482b4308b1
Cihat Solak <cihatsolak@hotmail.com>
2023-04-02 21:04:27 +0300

    test: add test code for schedule library

--- libraries/schedule/Puzzle.Lib.Hangfire/Infrastructure/Settings/HangfireSetting.cs
+++ libraries/schedule/Puzzle.Lib.Hangfire/Infrastructure/Settings/HangfireSetting.cs
@@ -8,51 +8,51 @@
         /// <summary>
         /// Gets or sets the connection string for the storage.
         /// </summary>
-        public string ConnectionString { get; init; }
+        public required string ConnectionString { get; init; }
 
         /// <summary>
         /// Gets or sets the expiration timeout in days for a job.
         /// </summary>
-        public int JobExpirationTimeoutDay { get; init; }
+        public required int JobExpirationTimeoutDay { get; init; }
 
         /// <summary>
         /// Gets or sets the interval in minutes to poll queues for jobs.
         /// </summary>
-        public int QueuePollIntervalMinute { get; init; }
+        public required int QueuePollIntervalMinute { get; init; }
 
         /// <summary>
         /// Gets or sets the maximum timeout in minutes for a command batch.
         /// </summary>
-        public int CommandBatchMaxTimeoutMinute { get; init; }
+        public required int CommandBatchMaxTimeoutMinute { get; init; }
 
         /// <summary>
         /// Gets or sets the number of attempts to automatically retry a failed job.
         /// </summary>
-        public int GlobalAutomaticRetryAttempts { get; init; }
+        public required int GlobalAutomaticRetryAttempts { get; init; }
 
         /// <summary>
         /// Gets or sets the relative path of the Hangfire dashboard.
         /// </summary>
-        public string DashboardPath { get; init; }
+        public required string DashboardPath { get; init; }
 
         /// <summary>
         /// Gets or sets the title of the Hangfire dashboard.
         /// </summary>
-        public string DashboardTitle { get; init; }
+        public required string DashboardTitle { get; init; }
 
         /// <summary>
         /// Gets or sets the relative path of the back button on the dashboard.
         /// </summary>
-        public string BackButtonPath { get; init; }
+        public required string BackButtonPath { get; init; }
 
         /// <summary>
         /// Gets or sets the username for Hangfire dashboard authorization.
         /// </summary>
-        public string Username { get; init; }
+        public required string Username { get; init; }
 
         /// <summary>
         /// Gets or sets the password for Hangfire dashboard authorization.
         /// </summary>
-        public string Password { get; init; }
+        public required string Password { get; init; }
     }
 }
