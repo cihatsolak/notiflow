045ac79fc42ac05cc5e5673248d1fc8a6729545e
Cihat Solak <cihatsolak@hotmail.com>
2023-04-04 00:41:46 +0300

    refactor: add formatter classes

---
+++ libraries/shared/Puzzle.Lib.Assistants/Formatters/DateTimeFormatterExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace Puzzle.Lib.Assistants.Formatters
+{
+    /// <summary>
+    /// Provides extension methods to format date and time values to string representations using custom formats.
+    /// </summary>
+    public static class DateTimeFormatterExtensions
+    {
+        /// <summary>
+        /// Converts the given DateTime value to a formatted string representation of the date and time in "dd/MM/yyyy HH:mm" format.
+        /// </summary>
+        /// <param name="dateTime">The DateTime value to be formatted.</param>
+        /// <returns>A string representation of the date and time in "dd/MM/yyyy HH:mm" format.</returns>
+        public static string ToDateTimeFormat(this DateTime dateTime)
+        {
+            return dateTime.ToString("dd/MM/yyyy HH:mm");
+        }
+
+        /// <summary>
+        /// Converts the given DateTime value to a formatted string representation of the date in "dd/MM/yyyy" format.
+        /// </summary>
+        /// <param name="dateTime">The DateTime value to be formatted.</param>
+        /// <returns>A string representation of the date in "dd/MM/yyyy" format.</returns>
+        public static string ToDateFormat(this DateTime dateTime)
+        {
+            return dateTime.ToString("dd/MM/yyyy");
+        }
+
+        /// <summary>
+        /// Converts the DateTime value to a string representation of the hour and minute in 24-hour format (e.g. "15:30").
+        /// </summary>
+        /// <param name="dateTime">The DateTime value to convert.</param>
+        /// <returns>A string representation of the hour and minute in 24-hour format.</returns>
+        public static string ToHourFormat(this DateTime dateTime)
+        {
+            return dateTime.ToString("HH:mm");
+        }
+    }
+}
---
+++ libraries/shared/Puzzle.Lib.Assistants/Formatters/PhoneFormatterExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Puzzle.Lib.Assistants.Formatters
+{
+    /// <summary>
+    /// Provides extension methods for formatting phone numbers.
+    /// </summary>
+    public static class PhoneFormatterExtensions
+    {
+        /// <summary>
+        /// Formats the phone number in GSM format.
+        /// </summary>
+        /// <param name="phone">The phone number to format.</param>
+        /// <returns>The phone number formatted in GSM format. {0:0 ### ### ## ##}</returns>
+        public static string ToGsmFormat(this string phone)
+        {
+            return string.Format("{0:0 ### ### ## ##}", phone);
+        }
+    }
+}
