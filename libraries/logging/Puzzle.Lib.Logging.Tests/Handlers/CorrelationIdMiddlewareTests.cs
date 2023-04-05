e33d521d37a51926c2a424dc2f97ea12d88b52c2
Cihat Solak <cihatsolak@hotmail.com>
2023-04-05 23:35:38 +0300

    test: add test code for correlation id middleware

---
+++ libraries/logging/Puzzle.Lib.Logging.Tests/Handlers/CorrelationIdMiddlewareTests.cs
@@ -0,0 +1,61 @@
+﻿namespace Puzzle.Lib.Logging.Tests.Handlers
+{
+    public class CorrelationIdMiddlewareTests
+    {
+        [Fact]
+        public async Task InvokeAsync_ShouldAddCorrelationIdHeaderToRequest_WhenHeaderNotExists()
+        {
+            // Arrange
+            var httpContext = new DefaultHttpContext();
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            httpContextAccessorMock.Setup(x => x.HttpContext).Returns(httpContext);
+            var next = new RequestDelegate(context => Task.CompletedTask);
+            var middleware = new CorrelationIdMiddleware(next, httpContextAccessorMock.Object);
+
+            // Act
+            await middleware.InvokeAsync(httpContext);
+
+            // Assert
+            Assert.True(httpContext.Request.Headers.TryGetValue("x-correlation-id", out var values));
+            Assert.NotNull(values.First());
+        }
+
+        [Fact]
+        public async Task InvokeAsync_ShouldAddCorrelationIdHeaderToResponse_WhenHeaderNotExists()
+        {
+            // Arrange
+            var httpContext = new DefaultHttpContext();
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            httpContextAccessorMock.Setup(x => x.HttpContext).Returns(httpContext);
+            var next = new RequestDelegate(context => Task.CompletedTask);
+            var middleware = new CorrelationIdMiddleware(next, httpContextAccessorMock.Object);
+
+            // Act
+            await middleware.InvokeAsync(httpContext);
+
+            // Assert
+            Assert.True(httpContext.Response.Headers.ContainsKey("x-correlation-id"));
+            Assert.NotEmpty(httpContext.Response.Headers["x-correlation-id"]);
+        }
+
+        [Fact]
+        public async Task InvokeAsync_ShouldAddCorrelationIdHeaderToRequestAndResponse_WhenHeaderExists()
+        {
+            // Arrange
+            var correlationId = "test-correlation-id";
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Headers.Add("x-correlation-id", correlationId);
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            httpContextAccessorMock.Setup(x => x.HttpContext).Returns(httpContext);
+            var next = new RequestDelegate(context => Task.CompletedTask);
+            var middleware = new CorrelationIdMiddleware(next, httpContextAccessorMock.Object);
+
+            // Act
+            await middleware.InvokeAsync(httpContext);
+
+            // Assert
+            Assert.Equal(correlationId, httpContext.Request.Headers["x-correlation-id"]);
+            Assert.Equal(correlationId, httpContext.Response.Headers["x-correlation-id"]);
+        }
+    }
+}
---
+++ libraries/logging/Puzzle.Lib.Logging.Tests/Usings.cs
@@ -0,0 +1,4 @@
+global using Microsoft.AspNetCore.Http;
+global using Moq;
+global using Puzzle.Lib.Logging.Middlewares;
+global using Xunit;
