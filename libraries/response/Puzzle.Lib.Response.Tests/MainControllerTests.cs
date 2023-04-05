6af5b28e246fe6c331d37f7ee8527da1769413d6
Cihat Solak <cihatsolak@hotmail.com>
2023-04-05 23:41:48 +0300

    test: add test code for response library

---
+++ libraries/response/Puzzle.Lib.Response.Tests/MainControllerTests.cs
@@ -0,0 +1,27 @@
+namespace Puzzle.Lib.Response.Tests
+{
+    public class MainControllerTests
+    {
+        [Fact]
+        public void MainController_ShouldHaveAttributes()
+        {
+            // Arrange
+            var controllerType = typeof(MainController);
+            var expectedAttributes = new List<Type>()
+            {
+                typeof(ApiControllerAttribute),
+                typeof(ProducesAttribute),
+                typeof(ConsumesAttribute),
+                typeof(ProducesResponseTypeAttribute),
+                typeof(ProducesResponseTypeAttribute),
+                typeof(ControllerAttribute)
+            };
+
+            // Act
+            var attributes = controllerType.GetCustomAttributes(true).Select(attributes => attributes.GetType()).ToList();
+
+            // Assert
+            Assert.Equal(expectedAttributes, attributes);
+        }
+    }
+}
