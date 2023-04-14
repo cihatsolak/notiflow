545ee78dc0463822b47e6fb55dcc6688587bec63
Cihat Solak <cihatsolak@hotmail.com>
2023-04-14 22:29:00 +0300

    refactor: expand database configuration option

--- libraries/database/Puzzle.Lib.Database/IOC/ServiceCollectionContainerBuilderExtensions.cs
+++ libraries/database/Puzzle.Lib.Database/IOC/ServiceCollectionContainerBuilderExtensions.cs
@@ -1,89 +1,54 @@
 ﻿namespace Puzzle.Lib.Database.IOC
 {
     /// <summary>
-    /// Extension methods for setting up MVC services in an <see cref="IServiceCollection" />.
+    /// Provides extension methods for configuring SQL database in IServiceCollection.
     /// </summary>
     public static class ServiceCollectionContainerBuilderExtensions
     {
         /// <summary>
-        /// Add postgresql provider as database context
+        /// Adds a PostgreSQL database to the service collection for the specified DbContext type and configuration key.
         /// </summary>
-        /// <typeparam name="TDbContext">type of db dbcontext</typeparam>
-        /// <param name="services">type of built-in service collection interface</param>
-        /// <param name="contextName">database context class name</param>
-        /// <param name="isSplitQuery">split query behavior</param>
-        /// <seealso cref="https://docs.microsoft.com/en-us/ef/core/dbcontext-configuration/"/>
-        /// <returns>type of built-in service collection</returns>
-        /// <exception cref="ArgumentNullException">when the service provider cannot be built</exception>
-        public static IServiceCollection AddPostgreSql<TDbContext>(this IServiceCollection services, string contextName, bool isSplitQuery = true) where TDbContext : DbContext
+        /// <typeparam name="TDbContext">The type of the DbContext.</typeparam>
+        /// <param name="services">The IServiceCollection instance to add the PostgreSQL database to.</param>
+        /// <param name="configKey">The configuration key for the PostgreSQL database settings.</param>
+        /// <returns>The modified IServiceCollection instance.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the service provider is null.</exception>
+        public static IServiceCollection AddPostgreSql<TDbContext>(this IServiceCollection services, string configKey) where TDbContext : DbContext
         {
             IServiceProvider serviceProvider = services.BuildServiceProvider();
             ArgumentNullException.ThrowIfNull(serviceProvider);
 
-            var webHostEnvironment = serviceProvider.GetRequiredService<IWebHostEnvironment>();
+            bool isProductionEnvironment = serviceProvider.GetRequiredService<IWebHostEnvironment>().IsProduction();
+
             IConfiguration configuration = serviceProvider.GetRequiredService<IConfiguration>();
+            SqlSetting sqlSetting = configuration.GetRequiredSection(configKey).Get<SqlSetting>();
 
             services.AddDbContextPool<TDbContext>(contextOptions =>
             {
-                ConfigureWarnings(contextOptions);
-                ConfigureLog(contextOptions, webHostEnvironment);
+                contextOptions.ConfigureCustomWarnings();
+                contextOptions.ConfigureCustomLogs(isProductionEnvironment);
 
-                contextOptions.UseLazyLoadingProxies(false);
-                contextOptions.UseNpgsql(configuration.GetConnectionString(contextName), sqlOptions =>
+                contextOptions.UseNpgsql(sqlSetting.ConnectionString, sqlOptions =>
                 {
-                    sqlOptions.MigrationsAssembly(typeof(TDbContext).Assembly.FullName);
+                    sqlOptions.MigrationsAssembly(Assembly.GetCallingAssembly().FullName);
                     sqlOptions.CommandTimeout((int)TimeSpan.FromSeconds(60).TotalSeconds);
 
-
-                    if (isSplitQuery)
+                    if (sqlSetting.IsSplitQuery)
                     {
                         sqlOptions.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
                     }
                 });
+
+                contextOptions.UseLazyLoadingProxies(false);
+                contextOptions.UseQueryTrackingBehavior(QueryTrackingBehavior.TrackAll);
             });
 
-            ConfigureException(services, webHostEnvironment);
-
-            return services;
-        }
-
-        /// <summary>
-        /// Entity repository
-        /// </summary>
-        /// <param name="services">type of built-in service collection interface</param>
-        /// <returns>type of built-in service collection</returns>
-        [Obsolete("It is suitable for use in entity framework 6 and lower versions.")]
-        public static IServiceCollection AddEfEntityRepository(IServiceCollection services)
-        {
-            services.TryAddScoped(typeof(IEf6EntityRepository<>), typeof(Ef6EntityRepository<>));
-            services.TryAddScoped<IBaseUnitOfWork, BaseUnitOfWork>();
-
-            return services;
-        }
-
-        private static void ConfigureWarnings(DbContextOptionsBuilder contextOptions)
-        {
-            contextOptions.ConfigureWarnings(warnings => warnings.Ignore(CoreEventId.DetachedLazyLoadingWarning));
-            contextOptions.ConfigureWarnings(warnings => warnings.Ignore(CoreEventId.PossibleIncorrectRequiredNavigationWithQueryFilterInteractionWarning));
-            contextOptions.ConfigureWarnings(warnings => warnings.Ignore(CoreEventId.SensitiveDataLoggingEnabledWarning));
-            contextOptions.ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.BoolWithDefaultWarning));
-            contextOptions.ConfigureWarnings(warnings => warnings.Ignore(CoreEventId.RowLimitingOperationWithoutOrderByWarning));
-        }
-
-        private static void ConfigureLog(DbContextOptionsBuilder contextOptions, IWebHostEnvironment webHostEnvironment)
-        {
-            contextOptions.EnableDetailedErrors(!webHostEnvironment.IsProduction());
-            contextOptions.EnableSensitiveDataLogging(!webHostEnvironment.IsProduction());
-            contextOptions.LogTo(Console.WriteLine, LogLevel.Warning);
-            contextOptions.UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()));
-        }
-
-        private static void ConfigureException(IServiceCollection services, IWebHostEnvironment webHostEnvironment)
-        {
-            if (!webHostEnvironment.IsProduction())
+            if (!isProductionEnvironment)
             {
                 services.AddDatabaseDeveloperPageExceptionFilter();
             }
+
+            return services;
         }
     }
 }
---
+++ libraries/database/Puzzle.Lib.Database/Infrastructure/Settings/SqlSetting.cs
@@ -0,0 +1,18 @@
+﻿namespace Puzzle.Lib.Database.Infrastructure.Settings
+{
+    /// <summary>
+    /// Represents a SQL setting, containing a connection string and a flag indicating whether the query should be split.
+    /// </summary>
+    internal sealed record SqlSetting
+    {
+        /// <summary>
+        /// Gets or sets the connection string.
+        /// </summary>
+        public required string ConnectionString { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the query should be split.
+        /// </summary>
+        public required bool IsSplitQuery { get; set; }
+    }
+}
