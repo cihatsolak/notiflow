39fd7994c3fe7d520acd28ddccf00656082bce97
Cihat Solak <cihatsolak@hotmail.com>
2023-05-20 21:34:49 +0300

    feat: add projection services (email, notification, text message)

---
+++ src/projections/Notiflow.Projections.TextMessageService/Program.cs
@@ -0,0 +1,10 @@
+using Notiflow.Projections.TextMessageService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
