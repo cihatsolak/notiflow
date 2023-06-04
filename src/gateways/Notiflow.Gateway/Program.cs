f9fcf878f8ff7362608d1869d44fb916c2b44fe3
Cihat Solak <cihatsolak@hotmail.com>
2023-06-04 23:56:12 +0300

    feat: configure ocelot for service redirects

--- src/gateways/Notiflow.Gateway/Program.cs
+++ src/gateways/Notiflow.Gateway/Program.cs
@@ -1,17 +1,28 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddJsonFile($"ocelot.global.json", true, true)
+    .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.SwaggerEndpoint.json", true, true)
+    .AddEnvironmentVariables();
 
 builder.Services.AddControllers();
+builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseSwaggerForOcelotUI(opt =>
+{
+    opt.PathToSwaggerGenerator = "/swagger/docs";
+});
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+await app.UseOcelot();
 
 app.Run();
