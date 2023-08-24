bd34d46661ddc634dad864776f87e06316765136
Cihat Solak <cihatsolak@hotmail.com>
2023-08-25 00:57:16 +0300

    feat: get notification history by id

--- src/services/backoffice/presentation/Notiflow.Backoffice.API/Notiflow.Backoffice.API/Controllers/BaseApiController.cs
+++ src/services/backoffice/presentation/Notiflow.Backoffice.API/Notiflow.Backoffice.API/Controllers/BaseApiController.cs
@@ -1,6 +1,8 @@
 ﻿namespace Notiflow.Backoffice.API.Controllers;
 
 [Route("api/[controller]")]
+[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+[ProducesResponseType(typeof(Response<EmptyResponse>), StatusCodes.Status500InternalServerError)]
 public class BaseApiController : MainController
 {
     private ISender _sender = null!;
