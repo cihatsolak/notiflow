ae54abfc009a078cad3de01e0a77dfa9491279a1
Cihat Solak <cihatsolak@hotmail.com>
2023-09-09 12:34:37 +0300

    fix: refactor seed manager class

--- src/services/backoffice/infrastructure/Notiflow.Backoffice.Persistence/Seeds/SeedManager.cs
+++ src/services/backoffice/infrastructure/Notiflow.Backoffice.Persistence/Seeds/SeedManager.cs
@@ -2,35 +2,39 @@
 
 internal static class SeedManager
 {
-    internal static async Task SeedAsync(this IServiceCollection services)
+    internal static async Task SeedAsync(this IServiceCollection services, CancellationToken cancellationToken = default)
     {
-        IServiceProvider serviceProvider = services.BuildServiceProvider();
-        ArgumentNullException.ThrowIfNull(serviceProvider);
+        EnsureNotNull(services);
 
-        var notiflowDbContext = serviceProvider.GetRequiredService<NotiflowDbContext>();
+        var notiflowDbContext = services.BuildServiceProvider().GetRequiredService<NotiflowDbContext>();
 
-        bool isConnected = await notiflowDbContext.Database.CanConnectAsync(CancellationToken.None);
+        bool isConnected = await notiflowDbContext.Database.CanConnectAsync(cancellationToken);
         if (!isConnected)
         {
-            Debug.WriteLine("Could not connect to database. The database may not be available.");
+            Debug.WriteLine("[SeedManager] Could not connect to database. The database may not be available.");
             return;
         }
 
-        bool isExists = await notiflowDbContext.Customers.IgnoreQueryFilters().AnyAsync();
+        bool isExists = await notiflowDbContext.Customers.IgnoreQueryFilters().AnyAsync(cancellationToken);
         if (isExists)
         {
-            Debug.WriteLine("There is no need for migration as there is tenant information in the database.");
+            Debug.WriteLine("[SeedManager] There is no need for migSration as there is tenant information in the database.");
             return;
         }
 
         try
         {
-            await notiflowDbContext.Customers.AddRangeAsync(SeedData.GenerateCustomers());
-            await notiflowDbContext.SaveChangesAsync(CancellationToken.None);
+            await notiflowDbContext.Customers.AddRangeAsync(SeedData.GenerateCustomers(), cancellationToken);
+            await notiflowDbContext.SaveChangesAsync(cancellationToken);
         }
         finally
         {
             await notiflowDbContext.DisposeAsync();
         }
+    }
+
+    private static void EnsureNotNull(IServiceCollection services)
+    {
+        ArgumentNullException.ThrowIfNull(services);
     }
 }
