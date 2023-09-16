5e33ea6f2a89aa9a348c8a42f15328a29adf7985
Cihat Solak <cihatsolak@hotmail.com>
2023-09-16 22:20:24 +0300

    feat: masstransit container builder extensions

---
+++ src/projections/Notiflow.Projections.NotificationService/MassTransitContainerBuilderExtensions.cs
@@ -0,0 +1,40 @@
+﻿namespace Notiflow.Projections.NotificationService;
+
+internal static class MassTransitContainerBuilderExtensions
+{
+    internal static IServiceCollection AddCustomMassTransit(this IServiceCollection services)
+    {
+        IServiceProvider serviceProvider = services.BuildServiceProvider();
+        IConfiguration configuration = serviceProvider.GetRequiredService<IConfiguration>();
+        RabbitMqStandaloneSetting rabbitMqStandaloneSetting = configuration.GetRequiredSection(nameof(RabbitMqStandaloneSetting)).Get<RabbitMqStandaloneSetting>();
+
+        services.AddMassTransit(serviceCollectionBusConfigurator =>
+        {
+            serviceCollectionBusConfigurator.SetKebabCaseEndpointNameFormatter();
+
+            serviceCollectionBusConfigurator.AddConsumer<NotificationDeliveredEventConsumer>();
+            serviceCollectionBusConfigurator.AddConsumer<NotificationNotDeliveredEventConsumer>();
+
+            serviceCollectionBusConfigurator.UsingRabbitMq((busRegistrationContext, rabbitMqBusFactoryConfigurator) =>
+            {
+                rabbitMqBusFactoryConfigurator.Host(new Uri(rabbitMqStandaloneSetting.HostAddress), "/", hostConfigurator =>
+                {
+                    hostConfigurator.Username(rabbitMqStandaloneSetting.Username);
+                    hostConfigurator.Password(rabbitMqStandaloneSetting.Password);
+                });
+
+                rabbitMqBusFactoryConfigurator.ReceiveEndpoint(RabbitQueueName.NOTIFICATION_DELIVERED_EVENT_QUEUE, options =>
+                {
+                    options.ConfigureConsumer<NotificationDeliveredEventConsumer>(busRegistrationContext);
+                });
+
+                rabbitMqBusFactoryConfigurator.ReceiveEndpoint(RabbitQueueName.NOTIFICATION_NOT_DELIVERED_EVENT_QUEUE, options =>
+                {
+                    options.ConfigureConsumer<NotificationNotDeliveredEventConsumer>(busRegistrationContext);
+                });
+            });
+        });
+
+        return services;
+    }
+}
