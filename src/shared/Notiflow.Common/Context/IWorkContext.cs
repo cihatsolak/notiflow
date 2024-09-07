bc7a4b4f222f9ddfe0b851f2409f6766d8bc8ac2
Cihat Solak <cihatsolak@hotmail.com>
2024-09-07 16:17:48 +0300

    add work context

---
+++ src/shared/Notiflow.Common/Context/IWorkContext.cs
@@ -0,0 +1,9 @@
+﻿namespace Notiflow.Common.Context;
+
+public interface IWorkContext
+{
+    int TenantId { get; }
+    Guid TenantToken { get; }
+    string DefaultConnectionString { get; }
+    string ConnectionString<TDbContext>() where TDbContext : class, new();
+}
